5eaf061990bf74283b0fdc49c39418d97216d408
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-04 18:55:45 -0400

    write csv of candles

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -1,13 +1,16 @@
 "use strict"
 
 let exchanges = require("../lib/exchanges")
+let csv = require("../lib/csv")
 
 const run = async (args) => {
   console.log("ok")
 
   let candles = await exchanges.fetchCandles("tXRPUSD", "15m")
 
-  console.log(candles)
+  await csv.writeCsv(candles)
+
+  console.log("Wrote csv to", filePath)
 }
 
 run()
