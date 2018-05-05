968e1920edcaeb46702f60830e15254866cc26bb
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-05 06:08:01 -0400

    small refactor

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -1,23 +1,17 @@
 "use strict"
 
-const exchanges = require("../lib/exchanges")
+const { fetchCandles } = require("../lib/exchanges")
 const { writeCsv, cleanCsv } = require("../lib/csv")
 const { indicators } = require("../lib/indicators")
 
-const fields = ["long", "percentChange"]
-
 const run = async () => {
-  let candles = await exchanges.fetchCandles("tXRPUSD", "15m")
+  let candles = await fetchCandles("tXRPUSD", "15m")
 
-  fields.forEach(async field => {
-    candles = await indicators[field](candles)
+  Object.keys(indicators).forEach(async indicator => {
+    candles = await indicators[indicator](candles)
   })
 
   let data = await cleanCsv(candles)
-
-  //console.log(candles)
-
-  console.log(data)
 
   await writeCsv(data)
 }
