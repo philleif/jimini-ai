c56b6aeb893b717ed741896498898c37a17215ab
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-05 14:29:52 -0400

    support fetching multiple currency pairs

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -5,7 +5,10 @@ const { writeCsv, cleanCsv } = require("../lib/csv")
 const { indicators } = require("../lib/indicators")
 
 const run = async () => {
-  let candles = await fetchCandles("tXRPUSD", "15m")
+  let pairs = ["tXRPUSD", "tBTCUSD", "tEOSUSD", "tETHUSD", "tBCHUSD", "tLTCUSD"]
+  let timeframe = "15m"
+
+  let candles = await fetchCandles(pairs, timeframe)
 
   Object.keys(indicators).forEach(async indicator => {
     candles = await indicators[indicator](candles)
@@ -17,8 +20,25 @@ const run = async () => {
   await writeCsv(data, filePath, false)
 
   filePath = "tensorflow/data/candles-test.csv"
-  await writeCsv(data, filePath, false)
+  await writeCsv(
+    getRandomSubarray(data, Math.floor(data.length / 10)),
+    filePath,
+    false
+  )
+}
 
+function getRandomSubarray(arr, size) {
+  let shuffled = arr.slice(0),
+    i = arr.length,
+    temp,
+    index
+  while (i--) {
+    index = Math.floor((i + 1) * Math.random())
+    temp = shuffled[index]
+    shuffled[index] = shuffled[i]
+    shuffled[i] = temp
+  }
+  return shuffled.slice(0, size)
 }
 
 run()
