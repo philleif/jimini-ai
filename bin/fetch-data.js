27d40daf09172608ed53acbe4dc65becc0e67352
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-04 20:34:40 -0400

    add percent change indicator

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -4,14 +4,12 @@ const exchanges = require("../lib/exchanges")
 const { writeCsv } = require("../lib/csv")
 const { indicators } = require("../lib/indicators")
 
-const fields = [
-  "long"
-]
+const fields = ["long", "percentChange"]
 
 const run = async () => {
   let candles = await exchanges.fetchCandles("tXRPUSD", "15m")
 
-  fields.forEach(async (field) => {
+  fields.forEach(async field => {
     candles = await indicators[field](candles)
   })
 
