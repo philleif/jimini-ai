9a6b1c207cc2b3a1632ff83f586f9b1fbcb4f4a3
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-04 20:09:35 -0400

    add long strategy

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -1,12 +1,23 @@
 "use strict"
 
 const exchanges = require("../lib/exchanges")
-const csv = require("../lib/csv")
+const { writeCsv } = require("../lib/csv")
+const { indicators } = require("../lib/indicators")
+
+const fields = [
+  "long"
+]
 
 const run = async () => {
   let candles = await exchanges.fetchCandles("tXRPUSD", "15m")
 
-  await csv.writeCsv(candles)
+  fields.forEach(async (field) => {
+    candles = await indicators[field](candles)
+  })
+
+  console.log(candles)
+
+  await writeCsv(candles)
 }
 
 run()
