1b0623454c44169778454a4557f6efa1e34b7c98
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-05-15 06:43:37 -0400

    new framework for indicators

--- bin/fetch-data.js
+++ bin/fetch-data.js
@@ -5,40 +5,19 @@ const { writeCsv, cleanCsv } = require("../lib/csv")
 const { indicators } = require("../lib/indicators")
 
 const run = async () => {
-  let pairs = ["tXRPUSD", "tBTCUSD", "tEOSUSD", "tETHUSD", "tBCHUSD", "tLTCUSD"]
+  //let pairs = ["tXRPUSD", "tBTCUSD", "tEOSUSD", "tETHUSD", "tBCHUSD", "tLTCUSD"]
+  let pairs = ["tXRPUSD"]
   let timeframe = "15m"
-
   let candles = await fetchCandles(pairs, timeframe)
+  let filePath = "./data/candles.csv"
 
-  Object.keys(indicators).forEach(async indicator => {
+  for (let indicator of Object.keys(indicators)) {
     candles = await indicators[indicator](candles)
-  })
+  }
 
   let data = await cleanCsv(candles)
 
-  let filePath = "tensorflow/data/candles.csv"
-  await writeCsv(data, filePath, false)
-
-  filePath = "tensorflow/data/candles-test.csv"
-  await writeCsv(
-    getRandomSubarray(data, Math.floor(data.length / 10)),
-    filePath,
-    false
-  )
-}
-
-function getRandomSubarray(arr, size) {
-  let shuffled = arr.slice(0),
-    i = arr.length,
-    temp,
-    index
-  while (i--) {
-    index = Math.floor((i + 1) * Math.random())
-    temp = shuffled[index]
-    shuffled[index] = shuffled[i]
-    shuffled[i] = temp
-  }
-  return shuffled.slice(0, size)
+  await writeCsv(data, filePath, true)
 }
 
 run()
